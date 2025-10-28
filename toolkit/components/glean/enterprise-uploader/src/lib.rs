43dd933bc24decd128f66a7aa37763e97e1c7f07
Jan-Erik Rediger <jrediger@mozilla.com>
2025-10-28 22:12:03 +0100

    Implement the observer interface

--- toolkit/components/glean/enterprise-uploader/src/lib.rs
+++ toolkit/components/glean/enterprise-uploader/src/lib.rs
@@ -1,15 +1,19 @@
-use std::sync::OnceLock;
+use std::ffi::CStr;
 use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::OnceLock;
 
+use libc::c_char;
 use nserror::{nsresult, NS_OK};
-use xpcom::interfaces::nsIEnterpriseUploader;
+use xpcom::interfaces::{nsIEnterpriseUploader, nsISupports};
 use xpcom::{xpcom, xpcom_method, RefPtr};
 
 static FLIP_FLOP: OnceLock<AtomicBool> = OnceLock::new();
 
 #[unsafe(no_mangle)]
 #[expect(clippy::missing_safety_doc, reason = "Inherently unsafe.")]
-pub unsafe extern "C" fn new_enterprise_uploader_service(result: *mut *const nsIEnterpriseUploader) {
+pub unsafe extern "C" fn new_enterprise_uploader_service(
+    result: *mut *const nsIEnterpriseUploader,
+) {
     _ = FLIP_FLOP.set(AtomicBool::new(false));
 
     unsafe {
@@ -18,7 +22,7 @@ pub unsafe extern "C" fn new_enterprise_uploader_service(result: *mut *const nsI
     }
 }
 
-#[xpcom(implement(nsIEnterpriseUploader), atomic)]
+#[xpcom(implement(nsIEnterpriseUploader, nsIObserver), atomic)]
 pub struct EnterpriseUploader {}
 
 impl EnterpriseUploader {
@@ -30,5 +34,20 @@ impl EnterpriseUploader {
     fn get_is_alive(&self) -> Result<bool, nsresult> {
         let cell = FLIP_FLOP.wait();
         Ok(cell.fetch_not(Ordering::SeqCst))
+    }
+
+    xpcom_method!(observe => Observe(subject: *const nsISupports, topic: *const c_char, data: *const u16));
+    fn observe(
+        &self,
+        _subject: *const nsISupports,
+        topic: *const c_char,
+        _data: *const u16,
+    ) -> Result<nsresult, nsresult> {
+        unsafe {
+            let topic = CStr::from_ptr(topic);
+            let topic = topic.to_string_lossy();
+            eprintln!("EnterpriseUploader.observe. topic={topic}");
+            Ok(NS_OK)
+        }
     }
 }
